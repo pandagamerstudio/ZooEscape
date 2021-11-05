8a0490bb8661e2b01d6a818d0bfc0f4ff5b09877
Raulr00 <57095736+Raulr00@users.noreply.github.com>
2021-11-05 19:02:27 +0100

    Arreglado bug reiniciar nivel
    En photon los clientes no pueden modificar valores en llamadas rpc, se deben usar los raised events para estas tareas.

--- ProyectoAnimales/Assets/Scripts/PuertaScript.cs
+++ ProyectoAnimales/Assets/Scripts/PuertaScript.cs
@@ -26,10 +26,10 @@ public class PuertaScript : MonoBehaviourPun
                 string nombre = "";
                 switch(scene.name){
                     case "Level1":
-                        nombre = "Level3";
+                        nombre = "Level1";
                         break;
                     case "Level2":
-                        nombre = "Level3";
+                        nombre = "Level2";
                         break;
                     case "Level3": 
                         nombre = "Final";
