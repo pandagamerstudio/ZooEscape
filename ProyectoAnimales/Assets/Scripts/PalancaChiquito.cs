b371556ef3a234d9c8312f6c9ff8bf3657cde313
Raulr00 <raul2000pro@gmail.com>
2021-11-14 12:15:44 +0100

    [COMMIT] Arreglado tamaño de los jugadores al tocar palancas
    El código estaba pensado para una palanca no para dos, se ha cambiado todo el código. BUG: photon a veces no se entera de que la palanca ha sido pulsada, es el problema de que los jugadores tengan ambos una copia y no se use el instantiate, pero el codigo maneja esta situación para que se pueda seguir jugando

--- ProyectoAnimales/Assets/Scripts/PalancaChiquito.cs
+++ ProyectoAnimales/Assets/Scripts/PalancaChiquito.cs
@@ -6,12 +6,17 @@ using Photon.Realtime;
 
 public class PalancaChiquito : MonoBehaviourPun
 {
-    bool activada = false;
+  public  bool activada = false;
     float pulsar;
     private Animator animator;
     public GameObject[] players;
     bool playerIn, dentro;
     Collider2D collider;
+    [SerializeField]
+   public static bool unaVez;
+
+
+    GameObject miPersonaje,personajeDelOtro;
 
      void Awake()
     {
@@ -19,37 +24,53 @@ public class PalancaChiquito : MonoBehaviourPun
         playerIn = false;
         dentro = false;
         collider = GetComponent<BoxCollider2D>();
+        unaVez = false;
     }
+
+    private void Start()
+    {
+        StartCoroutine(seleccionarPersonajes());
+
+
+
+    }
+
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && !dentro)
+        if (collision.CompareTag("Player") )
         {
-            if (!playerIn){
-                players = GameObject.FindGameObjectsWithTag("Player");
-                playerIn = true;
-            }
+
             dentro = true;
             collider.enabled = false;
             if (activada) {
-                if (players[0].GetComponent<PlayerController>().id == collision.gameObject.GetComponent<PlayerController>().id){
+
+                cambiarTamano(collision.gameObject);
+              /*  if (players[0].GetComponent<PlayerController>().id == collision.gameObject.GetComponent<PlayerController>().id){
                     players[0].GetComponent<PlayerController>().changeScale(2);
                     players[1].GetComponent<PlayerController>().changeScale(1);
                 }else{
                     players[1].GetComponent<PlayerController>().changeScale(2);
                     players[0].GetComponent<PlayerController>().changeScale(1);
-                }
+                }*/
                 
                 animator.SetBool("Activada", false);
                 StartCoroutine(changeActivada(false));
             }
             else{
-                if (players[0].GetComponent<PlayerController>().id == collision.gameObject.GetComponent<PlayerController>().id){
+                
+                /*if (players[0].GetComponent<PlayerController>().id == collision.gameObject.GetComponent<PlayerController>().id){
                     players[0].GetComponent<PlayerController>().changeScale(1);
                     players[1].GetComponent<PlayerController>().changeScale(2);
                 }else{
                     players[1].GetComponent<PlayerController>().changeScale(1);
                     players[0].GetComponent<PlayerController>().changeScale(2);
-                }
+                }*/
+                //Caso que ambos jugadores tengan la misma escala(primera colisión), 
+ 
+              
+                    cambiarTamano(collision.gameObject);
+           
 
                 animator.SetBool("Activada", true);
                 StartCoroutine(changeActivada(true));
@@ -68,6 +89,65 @@ public class PalancaChiquito : MonoBehaviourPun
         yield return new WaitForSeconds(1.0f);
         activada = b;
         collider.enabled = true;
+    }
+
+    IEnumerator seleccionarPersonajes()
+    {
+        yield return new WaitForSeconds(1.0f);
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (players[0].GetComponent<PhotonView>().IsMine)
+        {
+            miPersonaje = players[0];
+            personajeDelOtro = players[1];
+        }
+        else
+        {
+            miPersonaje = players[1];
+            personajeDelOtro = players[0];
+        }
+    }
+
+    public void cambiarTamano(GameObject collision) {
+
+        Debug.Log("Tamaño cambiado, mi tamalo acutal "+ (int)Mathf.Abs(miPersonaje.transform.localScale.x));
+
+        if ((int)Mathf.Abs(miPersonaje.transform.localScale.x) == (int)Mathf.Abs(personajeDelOtro.transform.localScale.x))
+        {
+           
+            if (miPersonaje.GetComponent<PhotonView>().ViewID != collision.GetComponent<PhotonView>().ViewID)
+            {
+                if ((int)Mathf.Abs(miPersonaje.transform.localScale.x) == 1)
+                {
+                    miPersonaje.GetComponent<PlayerController>().changeScale(2);
+
+                }
+                else
+                {
+                    miPersonaje.GetComponent<PlayerController>().changeScale(1);
+
+
+                }
+            }
+
+        }
+        else {
+            if ((int)Mathf.Abs(miPersonaje.transform.localScale.x) == 1)
+            {
+                miPersonaje.GetComponent<PlayerController>().changeScale(2);
+
+            }
+            else
+            {
+                miPersonaje.GetComponent<PlayerController>().changeScale(1);
+
+
+            }
+        }
+
+
+       
+        Debug.Log("Mi tamaño cambiado " + (int)Mathf.Abs(miPersonaje.transform.localScale.x));
     }
 
 }
